c5b28572846fd82c8125a6b979d01b24b664c890
Marwa Jouili <marwa.jouili@isimg.tn>
2026-05-05 17:46:26 +0200

    Add Dockerized AI FinOps assistant with FastAPI, Streamlit UI and RAG integration

--- frontend/app.py
+++ frontend/app.py
@@ -6,8 +6,16 @@ st.title("💰 AI FinOps Assistant")
 question = st.text_input("Ask your cloud cost question")
 
 if st.button("Ask AI"):
-    response = requests.get(
-        "http://127.0.0.1:8000/ask",
-        params={"question": question}
-    )
-    st.write(response.json()["answer"])
+    try:
+        response = requests.get(
+            "http://backend:8000/ask",
+            params={"question": question}
+        )
+
+        if response.status_code == 200:
+            st.write(response.json()["answer"])
+        else:
+            st.error("Error from backend API")
+
+    except Exception as e:
+        st.error(f"Connection error: {e}")
